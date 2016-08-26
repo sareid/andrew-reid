6d3bb0ba337085769c335371c18076c8b6144af8
sareid <andrew.reid@flatironschool.com>
2016-08-25 22:16:22 -0400

    Adds index to project model

--- app/models/project.js
+++ app/models/project.js
@@ -6,5 +6,6 @@ export default DS.Model.extend({
   description: DS.attr('string'),
   github_url_2: DS.attr('string'),
   demo_url: DS.attr('string'),
-  blog_url: DS.attr('string')
+  blog_url: DS.attr('string'),
+  index: DS.attr('number')
 });
