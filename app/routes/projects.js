c73d4a0713421a1459f79103bd201090884b1382
sareid <andrew.reid@flatironschool.com>
2016-08-25 22:29:15 -0400

    Adds sorting by index to project index

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.findAll('project');
+    return this.store.findAll('project',{ reload : true }).then(projects => projects.sortBy('index').reverse());
   }
 });
